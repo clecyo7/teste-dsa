86708642a30dc059bd44a1ce42598f7326cd5fbd
clecyo7 <clecyocrf@gmail.com>
2023-12-19 22:29:37 -0300

    vesão 1.0

---
+++ wp-content/themes/temaDsa/index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div id="content">
+    <main id="main" role="main">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_content(); ?>
+                </article>
+        <?php endwhile;
+        endif; ?>
+    </main>
+</div>
+
+<?php get_footer(); ?>
